742ddabf99cf2dffce2cffe63f4e94188225eb41
Jimmy <jimmy@r3e.network>
2025-06-05 00:20:20 +0800

    style: Apply final formatting to generated protobuf files

--- secure-sign-rpc/src/startpb.rs
+++ secure-sign-rpc/src/startpb.rs
@@ -25,10 +25,10 @@ pub mod startup_service_client {
         dead_code,
         missing_docs,
         clippy::wildcard_imports,
-        clippy::let_unit_value,
+        clippy::let_unit_value
     )]
-    use tonic::codegen::*;
     use tonic::codegen::http::Uri;
+    use tonic::codegen::*;
     #[derive(Debug, Clone)]
     pub struct StartupServiceClient<T> {
         inner: tonic::client::Grpc<T>,
@@ -72,9 +72,8 @@ pub mod startup_service_client {
                     <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
                 >,
             >,
-            <T as tonic::codegen::Service<
-                http::Request<tonic::body::BoxBody>,
-            >>::Error: Into<StdError> + std::marker::Send + std::marker::Sync,
+            <T as tonic::codegen::Service<http::Request<tonic::body::BoxBody>>>::Error:
+                Into<StdError> + std::marker::Send + std::marker::Sync,
         {
             StartupServiceClient::new(InterceptedService::new(inner, interceptor))
         }
@@ -112,22 +111,14 @@ pub mod startup_service_client {
         pub async fn diffie_hellman(
             &mut self,
             request: impl tonic::IntoRequest<super::DiffieHellmanRequest>,
-        ) -> std::result::Result<
-            tonic::Response<super::DiffieHellmanResponse>,
-            tonic::Status,
-        > {
-            self.inner
-                .ready()
-                .await
-                .map_err(|e| {
-                    tonic::Status::unknown(
-                        format!("Service was not ready: {}", e.into()),
-                    )
-                })?;
+        ) -> std::result::Result<tonic::Response<super::DiffieHellmanResponse>, tonic::Status>
+        {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::unknown(format!("Service was not ready: {}", e.into()))
+            })?;
             let codec = tonic::codec::ProstCodec::default();
-            let path = http::uri::PathAndQuery::from_static(
-                "/startpb.StartupService/DiffieHellman",
-            );
+            let path =
+                http::uri::PathAndQuery::from_static("/startpb.StartupService/DiffieHellman");
             let mut req = request.into_request();
             req.extensions_mut()
                 .insert(GrpcMethod::new("startpb.StartupService", "DiffieHellman"));
@@ -136,22 +127,13 @@ pub mod startup_service_client {
         pub async fn start_signer(
             &mut self,
             request: impl tonic::IntoRequest<super::StartSignerRequest>,
-        ) -> std::result::Result<
-            tonic::Response<super::StartSignerResponse>,
-            tonic::Status,
-        > {
-            self.inner
-                .ready()
-                .await
-                .map_err(|e| {
-                    tonic::Status::unknown(
-                        format!("Service was not ready: {}", e.into()),
-                    )
-                })?;
+        ) -> std::result::Result<tonic::Response<super::StartSignerResponse>, tonic::Status>
+        {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::unknown(format!("Service was not ready: {}", e.into()))
+            })?;
             let codec = tonic::codec::ProstCodec::default();
-            let path = http::uri::PathAndQuery::from_static(
-                "/startpb.StartupService/StartSigner",
-            );
+            let path = http::uri::PathAndQuery::from_static("/startpb.StartupService/StartSigner");
             let mut req = request.into_request();
             req.extensions_mut()
                 .insert(GrpcMethod::new("startpb.StartupService", "StartSigner"));
@@ -166,7 +148,7 @@ pub mod startup_service_server {
         dead_code,
         missing_docs,
         clippy::wildcard_imports,
-        clippy::let_unit_value,
+        clippy::let_unit_value
     )]
     use tonic::codegen::*;
     /// Generated trait containing gRPC methods that should be implemented for use with StartupServiceServer.
@@ -175,17 +157,11 @@ pub mod startup_service_server {
         async fn diffie_hellman(
             &self,
             request: tonic::Request<super::DiffieHellmanRequest>,
-        ) -> std::result::Result<
-            tonic::Response<super::DiffieHellmanResponse>,
-            tonic::Status,
-        >;
+        ) -> std::result::Result<tonic::Response<super::DiffieHellmanResponse>, tonic::Status>;
         async fn start_signer(
             &self,
             request: tonic::Request<super::StartSignerRequest>,
-        ) -> std::result::Result<
-            tonic::Response<super::StartSignerResponse>,
-            tonic::Status,
-        >;
+        ) -> std::result::Result<tonic::Response<super::StartSignerResponse>, tonic::Status>;
     }
     #[derive(Debug)]
     pub struct StartupServiceServer<T> {
@@ -208,10 +184,7 @@ pub mod startup_service_server {
                 max_encoding_message_size: None,
             }
         }
-        pub fn with_interceptor<F>(
-            inner: T,
-            interceptor: F,
-        ) -> InterceptedService<Self, F>
+        pub fn with_interceptor<F>(inner: T, interceptor: F) -> InterceptedService<Self, F>
         where
             F: tonic::service::Interceptor,
         {
@@ -266,15 +239,11 @@ pub mod startup_service_server {
                 "/startpb.StartupService/DiffieHellman" => {
                     #[allow(non_camel_case_types)]
                     struct DiffieHellmanSvc<T: StartupService>(pub Arc<T>);
-                    impl<
-                        T: StartupService,
-                    > tonic::server::UnaryService<super::DiffieHellmanRequest>
-                    for DiffieHellmanSvc<T> {
+                    impl<T: StartupService> tonic::server::UnaryService<super::DiffieHellmanRequest>
+                        for DiffieHellmanSvc<T>
+                    {
                         type Response = super::DiffieHellmanResponse;
-                        type Future = BoxFuture<
-                            tonic::Response<Self::Response>,
-                            tonic::Status,
-                        >;
+                        type Future = BoxFuture<tonic::Response<Self::Response>, tonic::Status>;
                         fn call(
                             &mut self,
                             request: tonic::Request<super::DiffieHellmanRequest>,
@@ -311,15 +280,11 @@ pub mod startup_service_server {
                 "/startpb.StartupService/StartSigner" => {
                     #[allow(non_camel_case_types)]
                     struct StartSignerSvc<T: StartupService>(pub Arc<T>);
-                    impl<
-                        T: StartupService,
-                    > tonic::server::UnaryService<super::StartSignerRequest>
-                    for StartSignerSvc<T> {
+                    impl<T: StartupService> tonic::server::UnaryService<super::StartSignerRequest>
+                        for StartSignerSvc<T>
+                    {
                         type Response = super::StartSignerResponse;
-                        type Future = BoxFuture<
-                            tonic::Response<Self::Response>,
-                            tonic::Status,
-                        >;
+                        type Future = BoxFuture<tonic::Response<Self::Response>, tonic::Status>;
                         fn call(
                             &mut self,
                             request: tonic::Request<super::StartSignerRequest>,
@@ -353,23 +318,19 @@ pub mod startup_service_server {
                     };
                     Box::pin(fut)
                 }
-                _ => {
-                    Box::pin(async move {
-                        let mut response = http::Response::new(empty_body());
-                        let headers = response.headers_mut();
-                        headers
-                            .insert(
-                                tonic::Status::GRPC_STATUS,
-                                (tonic::Code::Unimplemented as i32).into(),
-                            );
-                        headers
-                            .insert(
-                                http::header::CONTENT_TYPE,
-                                tonic::metadata::GRPC_CONTENT_TYPE,
-                            );
-                        Ok(response)
-                    })
-                }
+                _ => Box::pin(async move {
+                    let mut response = http::Response::new(empty_body());
+                    let headers = response.headers_mut();
+                    headers.insert(
+                        tonic::Status::GRPC_STATUS,
+                        (tonic::Code::Unimplemented as i32).into(),
+                    );
+                    headers.insert(
+                        http::header::CONTENT_TYPE,
+                        tonic::metadata::GRPC_CONTENT_TYPE,
+                    );
+                    Ok(response)
+                }),
             }
         }
     }
